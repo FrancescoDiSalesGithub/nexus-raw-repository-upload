85e4d674d298656adda9f042698eb952734863cc
FrancescoDiSalesGithub <f.disales@hotmail.com>
2025-04-01 22:16:24 +0200

    fix upload and add download

---
+++ download.sh
@@ -0,0 +1,1 @@
+curl -L -u $1:$2 -vvvv -o $3  http://$4:$5/repository/$6$7
--- upload.sh
+++ upload.sh
@@ -11,8 +11,4 @@ port=$7
 repo=$8
 
 
-curl --location 'http://$ip:$port/service/rest/internal/ui/upload/$repo' \
--u $username:$password 
---form 'asset0=@"$file"' \
---form 'asset0.filename="$filename"' \
---form 'directory="$remote_dir"'
+curl --location 'http://$ip:$port/service/rest/internal/ui/upload/$repo' -u $username:$password -F 'asset0=@"$file"' -F 'asset0.filename="$filename"'  -F 'directory="$remote_dir"'
